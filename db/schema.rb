da93b9d2ae4152980c9b3f3a27f990ee8f60eda4
kristinamarkina <kr.markina@gmail.com>
2021-06-29 19:11:02 -0400

    Provider model created

---
+++ db/schema.rb
@@ -0,0 +1,50 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_06_29_230944) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "patients", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.date "dob"
+    t.string "sex"
+    t.string "ethnicity"
+    t.boolean "prev_etoh_use"
+    t.boolean "curr_etoh_use"
+    t.text "etoh_notes"
+    t.boolean "prev_smoking"
+    t.boolean "curr_smoking"
+    t.text "smoking_notes"
+    t.boolean "prev_drug_use"
+    t.boolean "curr_drug_use"
+    t.text "drug_notes"
+    t.text "illnesses"
+    t.text "surgeries"
+    t.text "allergies"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "providers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "sex"
+    t.string "languages"
+    t.string "specialty"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
